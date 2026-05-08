d0e0ff889cbc4c3b2b54a100351384bac944f677
SergBul85 <donetsk55555@gmail.com>
2026-05-08 15:07:37 +0300

    L_006_dep_inj

---
+++ src/main/java/spring_introduction/Test3.java
@@ -0,0 +1,15 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+//        Pet pet = new Cat();
+//        Person person = new Person(pet);
+//        person.callYourPet();
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        Pet pet = (Pet) context.getBean("myPet", Pet.class);
+        Person person = context.getBean("myPerson", Person.class);
+    }
+}
