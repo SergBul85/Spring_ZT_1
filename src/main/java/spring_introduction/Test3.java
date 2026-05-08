7ab6eda2840774d15d9f22d9aa6561604c3225a3
SergBul85 <donetsk55555@gmail.com>
2026-05-08 17:03:40 +0300

    L_007_dep_inj_constr

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -4,12 +4,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test3 {
     public static void main(String[] args) {
-//        Pet pet = new Cat();
-//        Person person = new Person(pet);
-//        person.callYourPet();
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Pet pet = (Pet) context.getBean("myPet", Pet.class);
+
         Person person = context.getBean("myPerson", Person.class);
+        person.callYourPet();
+
+
+        context.close();
     }
 }
