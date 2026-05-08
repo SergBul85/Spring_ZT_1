3694f0c482089a18eb8fcd5e6a64bf1522bf02db
SergBul85 <donetsk55555@gmail.com>
2026-05-08 20:12:57 +0300

    L_012_bean_scope

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,6 +1,18 @@
 package spring_introduction;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Dog implements Pet {
+
+    private String name;
+
+
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
 
     @Override
     public void say() {
