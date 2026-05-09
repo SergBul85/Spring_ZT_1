adc7b7e97a792514c7accc6b865bc30ef99d2c1e
SergBul85 <donetsk55555@gmail.com>
2026-05-09 19:40:19 +0300

    L_28_define_pointCut

--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class UniLibrary extends AbstractLibrary {
 
-    public void getBook(Book book) {
-        System.out.println("Мы берём книгу из UniLibrary " + book.getName());
+    public void getBook() {
+        System.out.println("Мы берём книгу из UniLibrary.");
     }
 
-    public String returnBook() {
-        System.out.println("Мы возвращаем книгу в UniLibrary.");
-        return "Ok";
-    }
+//    public String returnBook() {
+//        System.out.println("Мы возвращаем книгу в UniLibrary.");
+//        return "Ok";
+//    }
 
     public void getMagazine() {
         System.out.println("Мы берём журнал из UniLibrary.");
