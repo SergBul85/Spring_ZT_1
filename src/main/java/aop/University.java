192da84a787b9e409d5a0f31f0332062d433b96e
SergBul85 <donetsk55555@gmail.com>
2026-05-10 11:39:35 +0300

    L_32_After_Returning

---
+++ src/main/java/aop/University.java
@@ -0,0 +1,30 @@
+package aop;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class University {
+
+    private List<Student> students = new ArrayList<Student>();
+
+    public void addStudents() {
+        Student st1 = new Student("Zaur Tregulov", 4, 7.1);
+        Student st2 = new Student("Mikhail Ivanov", 2, 7.2);
+        Student st3 = new Student("Elena Sidorova", 1, 7.3);
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+
+    }
+
+    public List<Student> getStudents() {
+        System.out.println("Information from method getStudents");
+        students.forEach(s -> System.out.println("\t" + s));
+        return students;
+    }
+
+}
---
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -0,0 +1,36 @@
+package aop.aspects;
+
+import aop.Student;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Aspect
+public class UniversityLoggingAspect {
+
+    @Before("execution(* getStudents())")
+    public void beforeGetStudentsLoggingAdvice() {
+        System.out.println("BEFORE etStudentsLoggingAdvice: логируем получение списка студентовю");
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* getStudents())",
+            returning = "students")
+    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
+        Student student = students.get(0);
+
+        String nameSurname = student.getNameSurname();
+        nameSurname = "Mr. " + nameSurname;
+
+        student.setNameSurname(nameSurname);
+        student.setAvgGrage(1.1);
+
+        System.out.println("AFTER_RETURNING GetStudentsLoggingAdvice: логируем получение списка студентовю");
+    }
+
+}
