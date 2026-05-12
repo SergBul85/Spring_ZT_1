c32672d18819879becb4baeee0ea4d08316fd93d
SergBul85 <donetsk55555@gmail.com>
2026-05-12 22:45:52 +0300

    L_53_many_to_many_1

---
+++ src/main/java/hibernate_mani_to_many/entity/Child.java
@@ -0,0 +1,58 @@
+package hibernate_mani_to_many.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "children")
+public class Child {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String firstName;
+
+    @Column(name = "age")
+    private int age;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "child_section",
+            joinColumns = @JoinColumn(name = "child_id"),
+            inverseJoinColumns = @JoinColumn(name = "section_id"))
+    private List<Section> sections;
+
+    public Child() {
+    }
+
+    public Child(String firstName, int age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public void addSectionToChild(Section section) {
+        if (sections == null) {
+            sections = new ArrayList<>();
+        }
+        sections.add(section);
+//        section.addChildToSection(this);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
---
+++ src/main/java/hibernate_mani_to_many/entity/Section.java
@@ -0,0 +1,52 @@
+package hibernate_mani_to_many.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "section")
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String Name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "child_section",
+            joinColumns = @JoinColumn(name = "section_id"),
+            inverseJoinColumns = @JoinColumn(name = "child_id"))
+    private List<Child> children;
+
+    public Section() {
+    }
+
+    public Section(String name) {
+        Name = name;
+    }
+
+    public void addChildToSection(Child child) {
+        if (children == null) {
+            children = new ArrayList<>();
+        }
+        children.add(child);
+//        child.addSectionToChild(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", Name='" + Name + '\'' +
+                '}';
+    }
+}
