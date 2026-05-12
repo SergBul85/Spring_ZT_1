e28038b489f04b02c3d7092cb30dae221beefb33
SergBul85 <donetsk55555@gmail.com>
2026-05-12 21:07:27 +0300

    L_51_one_to_many_uni

---
+++ src/main/java/hibernate_one_to_many_uni/entity/Department.java
@@ -0,0 +1,58 @@
+package hibernate_one_to_many_uni.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String departmentName;
+
+    @Column(name = "max_salary")
+    private int maxSalary;
+
+    @Column(name = "min_salary")
+    private int minSalary;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "department_id")
+    private List<Employee> emps;
+
+    public Department() {
+    }
+
+    public Department(String departmentName, int maxSalary, int minSalary) {
+        this.departmentName = departmentName;
+        this.maxSalary = maxSalary;
+        this.minSalary = minSalary;
+    }
+
+    public void addEmployee(Employee employee) {
+        if (emps == null) {
+            emps = new ArrayList<>();
+        }
+        this.emps.add(employee);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", departmentName='" + departmentName + '\'' +
+                ", maxSalary=" + maxSalary +
+                ", minSalary=" + minSalary +
+                '}';
+    }
+}
